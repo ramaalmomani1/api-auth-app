3917c62652f475151adf5e827e38bf461886df2c
ramaalmoamni1 <raalmomani17197@eng.just.edu.jo>
2023-07-04 14:43:29 +0000

    starting the server & setting folders

---
+++ src/server.js
@@ -0,0 +1,29 @@
+'use strict';
+
+// require('dotenv').config();
+const express = require('express')
+const cors =require('cors')
+const app = express();
+app.use(cors())
+
+const pageNotFound = require('./errorhandler/404');
+const serverError = require('./errorhandler/500');
+const logger = require('./middleware/logger')
+//  const PORT = process.env.PORT
+
+
+// app.use(logger);
+
+app.use('*', pageNotFound);
+app.use(serverError);
+
+
+
+function start(port){
+ app.listen(port , ()=> console.log(`up and running on port: ${port}`))
+} 
+
+module.exports = {
+  app,
+  start
+}
